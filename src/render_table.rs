482216cf468c8226fb3ecbc7cff72795a8aa25cc
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-10-15 16:08:53 +0530

    remove once chain

--- src/render_table.rs
+++ src/render_table.rs
@@ -94,8 +94,8 @@ pub fn render<AST, Token, TranslatorStack>(_parser: &LR1_Parser<AST, Token, Tran
     });
 
     let index = once(String::new()).chain(_parser.LR1_automata.iter().map(|i| i.index.to_string()));
-    action_table.insert_column(0, once(String::new()).chain(index.clone()));
-    goto_table.insert_column(0, once(String::new()).chain(index));
+    action_table.insert_column(0, index.clone());
+    goto_table.insert_column(0, index);
 
     let mut a_table = action_table.build();
     a_table.with(Style::rounded());
