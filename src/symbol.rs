5ca29f75dba96d1089d9b9ed896feab2bdb8a9d0
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-12-09 14:52:18 +0530

    Add symbols data structure

--- src/symbol.rs
+++ src/symbol.rs
@@ -1,5 +1,7 @@
 use std::{fmt::Debug, hash::Hash};
 
+use indexmap::IndexMap;
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum Symbol {
     TERMINAL(String),
@@ -27,5 +29,46 @@ impl ToString for Symbol {
 impl From<&Symbol> for String {
     fn from(value: &Symbol) -> Self {
         value.to_string()
+    }
+}
+
+pub struct Symbols {
+    map: IndexMap<Symbol, usize>,
+    vec: Vec<Symbol>,
+    terminals: Vec<usize>,
+    non_terminals: Vec<usize>,
+}
+
+impl Symbols {
+    pub fn intern(&mut self, symbol: Symbol) -> usize {
+        if let Some(&id) = self.map.get(&symbol) {
+            return id;
+        }
+        let id = self.map.len();
+        self.map.insert(symbol.clone(), id);
+        self.vec.push(symbol);
+
+        id
+    }
+
+    #[must_use]
+    pub fn lookup(&self, id: usize) -> Symbol {
+        self.vec[id].clone()
+    }
+
+    #[must_use]
+    pub fn reverse_lookup(&self, symbol: &Symbol) -> Option<usize> {
+        self.map.get(symbol).map(|x| *x)
+    }
+
+    #[inline]
+    /// returns true if the id is terminal else false
+    pub fn terminal(&self, id: &usize) -> bool {
+        self.terminals.contains(&id)
+    }
+    #[inline]
+    /// returns true if the id is non_terminal else false
+    pub fn non_terminal(&self, id: &usize) -> bool {
+        self.non_terminals.contains(&id)
     }
 }
