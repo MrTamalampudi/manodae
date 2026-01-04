61661de73e06a8cdf12ff39c4291f20840333efb
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2026-01-04 18:57:20 +0530

    refactor grammar macro..
    the need of this commit is ..
    previously a production can accept a unintended combination of symbols
    on its right hand side.
    like
    
    * head -> terminal non_terminal ....
    * head -> non_terminal non.... terminal ...
    
    to fix this to
    
    * head -> (non_terminals)+
    * head -> [terminal]
    
    a grammar should have start production as first production..
    followed by non_terminal productions then terminal productions

--- src/grammar.rs
+++ src/grammar.rs
@@ -46,54 +46,178 @@ where
     }
 }
 
+//need to improve lot of repetetion
+
 #[macro_export]
-macro_rules! grammar {
+macro_rules! start_production {
     (
-        $(
-            $head:ident -> $(
-                $([$terminal:expr])?
-                $($non_terminal:ident)*
-                $({error:$error:literal})?
-                $({action:|$arg1:ident,$arg2:ident,$arg3:ident,$arg4:ident| $expr:block})?
-            )|+
-        );+;
-    ) => {{
-        let mut grammar = Grammar::new();
-        $({
-            $({let mut body_ : Vec<SymbolId> = Vec::new();
-                $(
-                    let terminal = Symbol::TERMINAL($terminal.to_string());
-                    let terminal_id = grammar.symbols.intern(terminal);
-                    body_.push(terminal_id);
-                )?
-                $(
-                    let non_terminal = Symbol::NONTERMINAL(stringify!($non_terminal).to_string());
-                    let non_terminal_id = grammar.symbols.intern(non_terminal);
-                    body_.push(non_terminal_id);
-                )*
-            let head = Symbol::NONTERMINAL(stringify!($head).to_string());
-            let head_id = grammar.symbols.intern(head);
+        $grammar:ident,
+        $($start_non_terminal:ident)+
+        $({ |$s_arg1:ident,$s_arg2:ident,$s_arg3:ident,$s_arg4:ident| $s_expr:block})?
+    ) => {
+            let mut body_ : Vec<SymbolId> = Vec::new();
+            $(
+                let non_terminal = Symbol::NONTERMINAL(stringify!($start_non_terminal).to_string());
+                let non_terminal_id = $grammar.symbols.intern(non_terminal);
+                body_.push(non_terminal_id);
+            )+
             #[allow(unused_mut)]
             let mut production = Production {
-                head: head_id,
+                head: SymbolId(2),
                 body: body_,
                 error_message: None,
                 action:None,
-                action_tokens : quote::quote!{Some(Rc::new(|ast,token_stack,tl_stack,errors| {}))}, //c1 macro expands to Rc::new(|ast,...| {})
-                index: grammar.productions.vec.len()
+                action_tokens : quote::quote!{Some(Rc::new(|ast,token_stack,tl_stack,errors| {}))},
+                index: $grammar.productions.vec.len()
             };
             $(
-              if $error.to_string().len() > 0 {
-                  production.error_message = Some($error.to_string());
-              }
+                production.action = Some(Rc::new(|$s_arg1,$s_arg2,$s_arg3,$s_arg4| $s_expr));
+                production.action_tokens = quote::quote!{Some(Rc::new(|$s_arg1,$s_arg2,$s_arg3,$s_arg4| $s_expr))};
             )?
-            $(
-                {production.action = Some(Rc::new(|$arg1,$arg2,$arg3,$arg4| $expr))}
-                {production.action_tokens = quote::quote!{Some(Rc::new(|$arg1,$arg2,$arg3,$arg4| $expr))}}
-            )?
+            $grammar.productions.intern(production);
+    }
+}
 
-            grammar.productions.intern(production);})+
-        })+
+#[macro_export]
+macro_rules! terminal_production {
+    (
+        $grammar:ident,
+        $head:ident,
+        [$terminal:expr]
+        $({ |$s_arg1:ident,$s_arg2:ident,$s_arg3:ident,$s_arg4:ident| $s_expr:block})?
+    ) => {
+
+        //lhs
+        let head = Symbol::NONTERMINAL(stringify!($head).to_string());
+        let head_id = $grammar.symbols.intern(head);
+
+        //rhs
+        let mut body_ : Vec<SymbolId> = Vec::new();
+        let terminal = Symbol::TERMINAL($terminal.to_string());
+        let terminal_id = $grammar.symbols.intern(terminal);
+        body_.push(terminal_id);
+
+        //production
+        #[allow(unused_mut)]
+        let mut production = Production {
+            head: head_id,
+            body: body_,
+            error_message: None,
+            action:None,
+            action_tokens : quote::quote!{Some(Rc::new(|ast,token_stack,tl_stack,errors| {}))},
+            index: $grammar.productions.vec.len()
+        };
+
+        $(
+            production.action = Some(Rc::new(|$s_arg1,$s_arg2,$s_arg3,$s_arg4| $s_expr));
+            production.action_tokens = quote::quote!{Some(Rc::new(|$s_arg1,$s_arg2,$s_arg3,$s_arg4| $s_expr))};
+        )?
+
+        $grammar.productions.intern(production);
+    };
+}
+
+#[macro_export]
+macro_rules! non_terminal_production {
+    (
+        $grammar:ident,
+        $head:ident,
+        $($non_terminal:ident)+
+        $({ |$s_arg1:ident,$s_arg2:ident,$s_arg3:ident,$s_arg4:ident| $s_expr:block})?
+    ) => {
+
+        //lhs
+        let head = Symbol::NONTERMINAL(stringify!($head).to_string());
+        let head_id = $grammar.symbols.intern(head);
+
+        //rhs
+        let mut body_ : Vec<SymbolId> = Vec::new();
+        $(
+            let non_terminal = Symbol::NONTERMINAL(stringify!($non_terminal).to_string());
+            let non_terminal_id = $grammar.symbols.intern(non_terminal);
+            body_.push(non_terminal_id);
+        )+
+
+        //production
+        #[allow(unused_mut)]
+        let mut production = Production {
+            head: head_id,
+            body: body_,
+            error_message: None,
+            action:None,
+            action_tokens : quote::quote!{Some(Rc::new(|ast,token_stack,tl_stack,errors| {}))},
+            index: $grammar.productions.vec.len()
+        };
+
+        $(
+            production.action = Some(Rc::new(|$s_arg1,$s_arg2,$s_arg3,$s_arg4| $s_expr));
+            production.action_tokens = quote::quote!{Some(Rc::new(|$s_arg1,$s_arg2,$s_arg3,$s_arg4| $s_expr))};
+        )?
+
+        $grammar.productions.intern(production);
+    };
+}
+
+#[macro_export]
+macro_rules! grammar{
+    (
+        Start -> $(
+            $($start_non_terminal:ident)+
+            $({|$s_arg1:ident,$s_arg2:ident,$s_arg3:ident,$s_arg4:ident| $s_expr:block})?
+        )|+;
+
+        $([non_terminal_productions])?
+
+        $(
+            $non_terminal_head:ident -> $(
+                $($non_terminal:ident)+
+                $({|$n_arg1:ident,$n_arg2:ident,$n_arg3:ident,$n_arg4:ident| $n_expr:block})?
+            )|+
+        ;)*
+
+        [terminal_productions]
+
+        $(
+            $terminal_head:ident -> $(
+                [$end_terminal:expr]
+                $({|$e_arg1:ident,$e_arg2:ident,$e_arg3:ident,$e_arg4:ident| $e_expr:block})?
+            )|+;
+        )+
+    ) => {{
+        let mut grammar = Grammar::new();
+        //start production
+        $(
+            $crate::start_production!(
+                grammar,
+                $($start_non_terminal)+
+                $({ |$s_arg1,$s_arg2,$s_arg3,$s_arg4| $s_expr })?
+            );
+        )+
+
+        //non_terminal_productions
+        $(
+            $(
+                $crate::non_terminal_production!(
+                    grammar,
+                    $non_terminal_head,
+                    $($non_terminal)+
+                    $({|$n_arg1,$n_arg2,$n_arg3,$n_arg4| $n_expr})?
+                );
+            )+
+        )?
+
+        //terminal_productions
+        $(
+            $(
+                $crate::terminal_production!(
+                    grammar,
+                    $terminal_head,
+                    [$end_terminal]
+                    $({|$e_arg1,$e_arg2,$e_arg3,$e_arg4| $e_expr})?
+                );
+            )+
+        )+
+
         grammar
     }}
 }
